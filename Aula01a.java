610fffb385c20e956053535b323739fbbc10f474
wellingtonrmacedo-alt <wellington.r.macedo@aluno.senai.br>
2026-08-15 16:56:51 -0300

    Aula1
    Curso de Java Avançado do senai - Java Programming

---
+++ Aula01a.java
@@ -0,0 +1,13 @@
+package aula01a;
+/**
+ * Nesses programas vamos estudar as 
+ * variaveis do tipo primitivo
+ * 
+ */
+public class Aula01a{
+
+    public static void main(String args[]){
+        System.out.println("Wellington Macedo");
+        System.out.println("=================");
+    }
+}
