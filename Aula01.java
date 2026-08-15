17f61f44f425e372ab9728f6125ade770b3ace1b
wellingtonrmacedo-alt <wellington.r.macedo@aluno.senai.br>
2026-08-15 17:13:15 -0300

    Java Programming senai
    Java avançado

---
+++ Aula01.java
@@ -0,0 +1,21 @@
+package aula01;
+/**
+ * Nesses programas vamos estudar as 
+ * variaveis do tipo primitivo
+ * 
+ */
+public class Aula01{
+
+    public static void main(String args[]){
+        System.out.println("Wellington Macedo");
+        System.out.println("=================");
+        System.out.println("--- Tipos Primitivos ---");
+        // Tipo byte
+        byte idadeAluno;
+        byte temperaturaAtual;
+        idadeAluno = 18;
+        temperaturaAtual = 27;
+        System.out.println("Idade do aluno: " + idadeAluno);
+        System.out.println("Temperatura atual: " + temperaturaAtual + " graus.");
+    }
+}
