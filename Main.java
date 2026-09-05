e7137d29afe202bb1630123d91ac8607e5e5619a
wellingtonrmacedo-alt <wellington.r.macedo@aluno.senai.br>
2026-09-05 10:45:58 -0300

    Atualização do exercício do sistema PDV
    Usando Orientação de Objetos em Java.
    Usei O JOptionPane

--- Main.java
+++ Main.java
@@ -1,55 +1,43 @@
-import javax.swing.JOptionPane;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class Main{
-    public static void main(String args[]){
 
-        String titulo = "Lojas Fantomas";
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
         SistemaLoja loja = new SistemaLoja();
-        loja.nome = JOptionPane.showInputDialog(null,"Digite seu nome: ",
-                titulo, 3 );
-        loja.produto = JOptionPane.showInputDialog(null, "Digite o produto: ",
-                titulo, 3);
-        loja.preco = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o preço: ",
-                titulo, 3));
-        loja.numParcelas = Integer.parseInt(JOptionPane.showInputDialog(null,
-                "Digite a quantidade de parcelas: ", titulo, 3));
-        loja.vencimentos = Integer.parseInt(JOptionPane.showInputDialog(null,
-                "Digite dia de vencimento: ", titulo, 3));
 
-        System.out.println("=".repeat(40));
-        System.out.println("          Lojas Fantomas        ");
-        System.out.println("=".repeat(40));
-        System.out.println("Cliente: " + loja.nome);
-        System.out.println("Produto: " + loja.produto);
-        System.out.println("Valor: R$ " + String.format("%.2f", loja.preco));
-        System.out.println("Parcelas: " + loja.numParcelas);
-        System.out.print("Valor da parcela: " + String.format("%.2f", loja.precoParcela()) +
-                " em " + loja.numParcelas + " vezes.\n");
-        System.out.println("=".repeat(40));
+        loja.entradaDados();
 
+        loja.imprimirCupom();
 
-        LocalDate data = LocalDate.of(2026, 8, loja.vencimentos);
+        LocalDate data = LocalDate.of(2026, 9, loja.vencimentos);
 
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        double valorParcela = loja.precoParcela();
+        if(loja.numParcelas == 1){
+            valorParcela = loja.aplicarDesconto();
+            System.out.println("Parcela Única com desconto de 10% a vista: "
+            + data.format(formato) + " - R$ " + String.format("%.2f", valorParcela));
+        }
 
         int parcela = 1;
 
         while(parcela <= loja.numParcelas){
-            System.out.println("Parcela " + parcela + ": "
-                    + data.format(formato) + " - R$ "
-                    + String.format("%.2f", loja.precoParcela()));
+            System.out.println("Parcela " + parcela + ": " + data.format(formato)
+            + " - R$ " + String.format("%.2f", valorParcela));
 
             data = data.plusMonths(1);
             parcela++;
         }
-        System.out.println("=".repeat(40));
-        System.out.println("Sistema de Loja SENAI GUARULHOS DUTRA");
-        System.out.println("=".repeat(40));
-        System.out.println("Muito Obrigado pela Compra!");
-        System.out.println("    Volte Sempre!    ");
-        System.out.println("=".repeat(40));
+
+        loja.rodape();
+
 
     }
 }
--- SistemaLoja.java
+++ SistemaLoja.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class SistemaLoja {
 
     String nome;
@@ -8,17 +10,55 @@ public class SistemaLoja {
     int vencimentos;
     double desconto;
 
+    public void entradaDados(){
+        String titulo = "Lojas Fantomas";
+        this.nome = JOptionPane.showInputDialog(null,
+                "Digite seu nome: ",
+                titulo, 3 );
+        this.produto = JOptionPane.showInputDialog(null,
+                "Digite o produto: ",
+                titulo, 3);
+        this.preco = Double.parseDouble(JOptionPane.showInputDialog(null,
+                "Digite o preço: ",
+                titulo, 3));
+        this.numParcelas = Integer.parseInt(JOptionPane.showInputDialog(null,
+                "Digite a quantidade de parcelas: ", titulo, 3));
+        this.vencimentos = Integer.parseInt(JOptionPane.showInputDialog(null,
+                "Digite um desse dia de vencimento: 05, 10, 15, 20, 25",
+                titulo, 3));
+    }
+
     public double precoParcela(){
         this.valorParcela = preco / numParcelas;
         return valorParcela;
     }
 
     public double aplicarDesconto(){
-        desconto = preco * 0.10;
+        desconto = preco - (preco * 0.10);
         return desconto;
     }
 
+    public void imprimirCupom(){
+        System.out.println("=".repeat(40));
+        System.out.println("        Lojas Fantomas        ");
+        System.out.println("=".repeat(40));
+        System.out.println("Cliente: " + this.nome);
+        System.out.println("Produto: " + this.produto);
+        System.out.println("Valor: " + String.format("%.2f", this.preco));
+        System.out.println("Parcelas: " + this.numParcelas);
+        System.out.println("Valor das parcela: " + String.format("%.2f",
+                this.precoParcela()) + " em "
+                + this.numParcelas + " vezes.\n");
+        System.out.println("=".repeat(40));
+    }
 
+    public void rodape(){
+        System.out.println("=".repeat(40));
+        System.out.println("Sistema de Loja SENAI GUARULHOS DUTRA");
+        System.out.println("=".repeat(40));
+        System.out.println("Muito Obrigado pela Compra!");
+        System.out.println("        Volte Sempre!        ");
+        System.out.println("=".repeat(40));
+    }
 
 }
-
